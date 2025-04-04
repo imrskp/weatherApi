fd70fd46b7b6436b686c015a07009ddd523f733d
Rohit <iamrskp@gmail.com>
2025-04-04 18:31:50 +0530

    weather api logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-// import { useState } from 'react'
-import './App.css'
-
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='bg-blue-600 text-white font-bold p-3 text-xl'>Har Har Mahadev</h1>
+      {/* <h1 className='bg-blue-600 text-white font-bold p-3 text-xl'>Har Har Mahadev</h1> */}
+      <nav className="bg-blue-600 h-[70px] w-auto flex items-center px-20">
+        <div className="bg-white rounded-xl px-4 py-2">
+          <p className="font-bold text-blue-600 cursor-pointer">Weather API</p>
+        </div>
+      </nav>
+
     </>
   )
 }
